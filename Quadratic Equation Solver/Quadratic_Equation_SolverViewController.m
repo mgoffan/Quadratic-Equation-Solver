bcde94e265d2bdb7ada845b84338091f79a59b3a
Martin Goffan <martinkpo12@gmail.com>
2011-08-29 22:58:20 -0300

    Interface needs improvement but calculation are correct

--- Quadratic Equation Solver/Quadratic_Equation_SolverViewController.m
+++ Quadratic Equation Solver/Quadratic_Equation_SolverViewController.m
@@ -73,19 +73,34 @@
 }
 
 - (IBAction)calc:(id)sender {
-    double a, b, c;
-    a = [aText.text doubleValue];
-    b = [bText.text doubleValue];
-    c = [cText.text doubleValue];
+    long double a, b, c;
+    a = aText.text.doubleValue;
+    b = bText.text.doubleValue;
+    c = cText.text.doubleValue;
     
-    double mb = b * -1;
-    double bsq = b * b;
-    double ac4 = 4 * a * c;
-    double a2 = 2 * a;
+    NSLog(@"1 %Lf %Lf %Lf",a,b,c);
     
-    double f = sqrt(bsq - ac4);
+    long double mb = b * -1;
+    long double b2 = b * b;
+    long double ac4 = 4 * a * c;
+    long double a2 = 2 * a;
     
-    l1.text = [NSString stringWithFormat:@"%d", (mb + f) / a2 ];
-    l2.text = [NSString stringWithFormat:@"%d", (mb - f) / a2 ];
+    NSLog(@"2 %Lf %Lf %Lf %Lf",mb, b2, ac4, a2);
+    
+    if (b2 == ac4) {l1.text = [NSString stringWithFormat:@"%Lf", mb / a2]; l2.text = l1.text;}
+    else if (b2 < ac4) l2.text = l1.text = @"NO SOLUTION";
+    else {
+        long double f = sqrt(b2 - ac4);
+        
+        NSLog(@"3 %Lf",f);
+        
+        long double mbf = mb + f;
+        long double mbf2 = mb -f;
+        
+        NSLog(@"4 %Lf %Lf",mbf, mbf2);
+        
+        l1.text = [NSString stringWithFormat:@"%Lf", mbf / a2 ];
+        l2.text = [NSString stringWithFormat:@"%Lf", mbf2 / a2 ];
+    }
 }
 @end
